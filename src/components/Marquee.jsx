c71a046fad92fef0090cfaba2feffb6a8ce8bbc2
Nidhish Zala <nidhish1132007@gmail.com>
2025-08-19 10:04:06 +0530

    second page animation + marquee improved

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 
 const Marquee = () => {
   return (
-    <div className='h-[20vh] w-full flex items-center justify-center overflow-hidden bg-black text-2xl font-semibold'>
-       <div class="scroller flex w-[60vw] h-30 text-white bg-gradient-to-r from-[rgba(0,0,0,0.1)] via-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.1)] overflow-hidden">
-          <div class="scroller-in mr-10 flex items-center justify-center gap-10 h-full bg-transparent">
+    <div className='h-[20vh] w-full flex items-center justify-center bg-black text-2xl font-semibold relative'>
+        <div className='absolute h-30 w-[60vw] bg-gradient-to-r from-black via-black/0 to-black z-3 overflow-hidden'></div>
+        <div className="scroller flex w-[60vw] h-30 text-white overflow-hidden">
+          <div className="scroller-in mr-10 flex items-center justify-center gap-10 h-full">
             <h2>TOPTRACER</h2>
             <h2>LESSONS</h2>
             <h2>ADVENTURE</h2>
             <h2>CAFE</h2>
             <h2>EVENTS</h2>
           </div>
-          <div class="scroller-in flex items-center justify-center gap-10 h-full">
+          <div className="scroller-in flex items-center justify-center gap-10 h-full">
             <h2>TOPTRACER</h2>
             <h2>LESSONS</h2>
             <h2>ADVENTURE</h2>
--- src/components/Ourstory.jsx
+++ src/components/Ourstory.jsx
@@ -1,25 +1,64 @@
 import React from 'react'
 import employee from '../assets/employee.avif'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {useGSAP} from '@gsap/react'
+import gsap from 'gsap'
+gsap.registerPlugin(ScrollTrigger);
+
 
 const Ourstory = () => {
+
+  useGSAP(() => {
+    gsap.from(".employee", {
+      opacity: 0,
+      scale: 0.5,
+      duration: 0.5,
+      scrollTrigger: {
+        trigger: ".employee",
+        start: "top 70%",
+        toggleActions: "play none none reverse"
+      }
+    });
+    gsap.from(".OurStoryText", {
+      opacity: 0,
+      y: 100,
+      duration: 0.3,
+      stagger: 0.3,
+      scrollTrigger: {
+        trigger: ".OurStoryText",
+        start: "top 70%",
+        toggleActions: "play none none reverse"
+      }
+    });
+  });
+
+
   return (
     <div className='flex items-center justify-center h-screen w-full bg-black text-white p-6 gap-30'>
       <div className='h-screen w-3/5 flex flex-col justify-center space-y-10'>
-        <h2>
+        <span className='overflow-hidden'>
+          <h2  className='OurStoryText'>
             At Growth Magnet Studio, we specialise in generating targeted and qualified leads and appointments based on your needs. 
         </h2>
-        <h2 className='text-xl font-bold'>
+        </span>
+        <span className='overflow-hidden'>
+          <h2 className='text-xl font-bold OurStoryText'>
             Our Story
         </h2>
-        <p>
+        </span>
+        <span className='overflow-hidden'>
+          <p className='OurStoryText'>
             We started as solar reps, developing a proven strategy to book appointments that quickly gained popularity among other reps. Realising the importance of consistent leads for business growth, we expanded into helping companies set up ad campaigns and appointment systems. Over the last five years, we've worked with 500+ reps and businesses, delivering steady leads and driving success.
         </p>
-         <button className="bg-red-600 text-white py-2 px-4 rounded-lg hover:bg-red-700 cursor-pointer w-50">
+        </span>
+         <span className='overflow-hidden'>
+          <button className="OurStoryText bg-red-600 text-white py-2 px-4 rounded-lg hover:bg-red-700 cursor-pointer w-50">
           Schedule a Call
         </button>
+         </span>
       </div>
       <div className='h-screen w-1/5'>
-        <img src={employee} alt="" className='h-full w-full object-contain'/>
+        <img src={employee} alt="" className='employee h-full w-full object-contain'/>
       </div>
     </div>
   )
