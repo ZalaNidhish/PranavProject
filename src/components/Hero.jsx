bdbc8741ff096a08d0776260210b71fcfc94403c
Nidhish Zala <nidhish1132007@gmail.com>
2025-08-19 22:20:58 +0530

    page1 content change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,40 +41,33 @@ const Hero = () => {
           Welcome to PIXEL PLUS MEDIA
         </h3>
         <h1 className="hero-title text-4xl w-[80vw] font-bold text-white leading-14">
-          {/* Guaranteed 75-100 Solar Appointments In the Next 90 Days With our AI Hybrid Appointment System */}
           <span className="text-red-500">Guaranteed</span>
           <span> </span>
-          <span>75-100</span>
+          <span>20-40</span>
           <span> </span>
-          <span>Solar</span>
+          <span>Epoxy</span>
           <span> </span>
-          <span>Appointments</span>
+          <span>Flooring</span>
           <span> </span>
-          <span className="text-red-500">In</span>
+          <span className="text-red-500">Leads</span>
           <span> </span>
-          <span className="text-red-500">the</span>
+          <span className="text-red-500">Every</span>
           <span> </span>
-          <span className="text-red-500">Next</span>
-          <span> </span>
-          <span className="text-red-500">90</span>
-          <span> </span>
-          <span className="text-red-500">Days</span>
+          <span className="text-red-500">Month</span>
           <span> </span>
           <span className="text-red-500">With</span>
           <span> </span>
           <span className="text-red-500">our</span>
           <span> </span>
           <span>AI</span>
-          <span> </span>
-          <span>Hybrid</span>
-          <span> </span>
-          <span>Appointment</span>
+          <span>-</span>
+          <span>Powered</span>
           <span> </span>
           <span>System</span>
         </h1>
         <p className="para text-[16px] text-gray-400 max-w-2xl">
-          An amalgamated approach where we use AI Agents, Call centre, FB Ads,
-          Human Agents & an AI Backed CRM.
+          Pixel Plus Media helps epoxy flooring businesses consistently book high-ticket projects without wasting time or money on outdated marketing.
+
         </p>
         <button className="button bg-red-600 text-white py-2 px-4 rounded-lg hover:bg-red-700 cursor-pointer">
           Book a Free Consultation
